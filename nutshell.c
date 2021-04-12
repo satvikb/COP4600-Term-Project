9349462668ac2575ece1b1fa95fc72b2e635c3e8
Satvik Borra <borrasatvik@gmail.com>
2021-04-12 04:13:18 -0400

    alias and env to maps, add other built ins

--- nutshell.c
+++ nutshell.c
@@ -9,46 +9,27 @@
 extern int yyparse (void);
 
 char *getcwd(char *buf, size_t size);
+void buildEnvs();
 
 int main()
 {
-    aliasIndex = 0;
-    varIndex = 0;
-    char cwd[PATH_MAX];
-    getcwd(cwd, sizeof(cwd)); // put the current working directory into cwd
-
-    strcpy(varTable.var[varIndex], "PWD"); // varIndex = 0
-    strcpy(varTable.word[varIndex], cwd);
-    varIndex++;
-    strcpy(varTable.var[varIndex], "HOME"); // varIndex = 1
-    strcpy(varTable.word[varIndex], cwd);
-    varIndex++;
-    strcpy(varTable.var[varIndex], "PROMPT"); // varIndex = 2
-    strcpy(varTable.word[varIndex], "nutshell-sb");
-    varIndex++;
-    strcpy(varTable.var[varIndex], "PATH"); // varIndex = 3
-    strcpy(varTable.word[varIndex], ".:/usr/bin");
-    varIndex++;
-
-    strcpy(aliasTable.name[aliasIndex], "."); // aliasIndex = 0
-    strcpy(aliasTable.word[aliasIndex], cwd);
-    aliasIndex++;
-
-    char *pointer = strrchr(cwd, '/');
-    while(*pointer != '\0') {
-        *pointer ='\0';
-        pointer++;
-    }
-    strcpy(aliasTable.name[aliasIndex], "..");  // aliasIndex = 1
-    strcpy(aliasTable.word[aliasIndex], cwd);
-    aliasIndex++;
+    buildEnvs();
 
     system("clear");
     while(1)
     {
-        printf("[%s]>> ", varTable.word[2]);
+        printf("[%s]>> ", &envMap["PROMPT"][0]);
         yyparse();
     }
 
     return 0;
+}
+
+void buildEnvs(){
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd)); // put the current working directory into cwd
+
+    envMap["PWD"] = cwd;
+    envMap["PATH"] = ".:/bin:/usr/bin";
+    envMap["PROMPT"] = "nutshell-sb";
 }
